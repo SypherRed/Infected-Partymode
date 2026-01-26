8e7f316402715808e6762ea41b9c7e2dd9a66b39
SypherRed <ext.sypher@gmail.com>
2026-01-26 21:18:59 +0100

    try to sync ownership claim

--- src/main/java/com/sypherred/infectedpartymode/party/PartySyncManager.java
+++ src/main/java/com/sypherred/infectedpartymode/party/PartySyncManager.java
@@ -101,6 +101,8 @@ public class PartySyncManager
     private void onHostClaimReceived(long memberId)
     {
         hostAuthorityManager.onHostClaim(memberId);
+        eventBus.post(PlayerStatesUpdated.INSTANCE);
+
     }
 
     /* =========================
